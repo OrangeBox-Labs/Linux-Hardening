0a76c62698400d4e02db35082839a4c280d0ad04
Felipe Roman <fromanm@gmail.com>
2026-06-09 21:25:03 -0400

    fix: sshd config

--- ssh-hardening-complete.sh
+++ ssh-hardening-complete.sh
@@ -301,7 +301,7 @@ Ciphers chacha20-poly1305@openssh.com,aes256-gcm@openssh.com,aes128-gcm@openssh.
 MACs hmac-sha2-256-etm@openssh.com,hmac-sha2-512-etm@openssh.com,umac-128-etm@openssh.com
 
 # Hardening adicional
-PasswordAuthentication no
+PasswordAuthentication yes
 PermitEmptyPasswords no
 ChallengeResponseAuthentication no
 KerberosAuthentication no
